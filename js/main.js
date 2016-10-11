b3990edbf377e77d7f2695321b04c2b8715f39d2
Matt Bontrager <matt.bontrager@gmail.com>
2016-10-11 09:50:47 -0700

    update license; add license links

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+/**
+ * HisStory (c) by Matt Bontrager
+ *
+ * HisStory is licensed under a
+ * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ *
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
+ */
+
 var development = true,
 	w = window,
 	store = w.localStorage,
